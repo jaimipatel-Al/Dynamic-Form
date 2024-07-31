81fdaf99b41a352abfaf554760681815f20bcb1d
Jaimi Patel <jaimip.albiorix@gmail.com>
2024-07-31 11:21:22 +0530

    ci : remove unused files

---
+++ src/views/CreateForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const backToList = () => {
+  router.push({ name: 'form-list' })
+}
+</script>
+
+<template>
+  <div>
+    <span class="back" @click="backToList">< Back</span>
+    <h1>Create Form</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+.back {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  cursor: pointer;
+  user-select: none;
+  font-weight: 500;
+}
+.back:hover {
+  color: blue;
+}
+</style>
---
+++ src/views/FormList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const createNewForm = () => {
+  router.push({ name: 'create-form' })
+}
+</script>
+
+<template>
+  <div>
+    <h1>Forms</h1>
+    <button @click="createNewForm">Create Form</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+button {
+  margin: 0 auto;
+  display: block;
+  font-size: 14px;
+  padding: 8px 20px;
+  font-weight: 600;
+  color: #fff;
+  background: black;
+  border-radius: 20px;
+  cursor: pointer;
+}
+</style>
